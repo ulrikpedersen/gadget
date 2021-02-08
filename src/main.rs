b3ee1e9daea57b80b48b7a78ca08aa46c3424aab
Ulrik Kofoed Pedersen <ulrikkofoed@gmail.com>
2021-02-08 22:15:14 +0000

    Adding stm32f3xx_hal crate

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,13 @@ use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch
 
 use cortex_m::asm;
 use cortex_m_rt::entry;
+use stm32f3xx_hal::{prelude::*, stm32, Delay, i2c};
+use stm32f3xx_hal::gpio::gpiob;
 
 #[entry]
 fn main() -> ! {
-    asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
-
+    let peripherals = stm32::Peripherals::take()::unwrap();
+    
     loop {
         // your code goes here
     }
