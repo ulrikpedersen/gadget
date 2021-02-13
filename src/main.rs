600db5b885bdf0bbac87d0b5fb0ac877ef6db601
Ulrik Kofoed Pedersen <ulrikkofoed@gmail.com>
2021-02-13 14:48:32 +0000

    code: prepare i2c bus for use
    Implemented simple loop with print and delay 1000ms

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,36 @@ use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch
 // use panic_itm as _; // logs messages over ITM; requires ITM support
 // use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
 
-use cortex_m::asm;
+//use cortex_m::asm;
 use cortex_m_rt::entry;
 use stm32f3xx_hal::{prelude::*, stm32, i2c};
-use stm32f3xx_hal::gpio::gpiob;
+//use stm32f3xx_hal::gpio::gpiob;
 use stm32f3xx_hal::delay::Delay;
+
+use cortex_m_semihosting::{hprintln, debug};
 
 #[entry]
 fn main() -> ! {
     let peripherals = stm32::Peripherals::take().unwrap();
-    
+    // rcc: reset clock control
+    let mut rcc = peripherals.RCC.constrain();
+    let core_peripherals = cortex_m::Peripherals::take().unwrap();
+    let mut flash = peripherals.FLASH.constrain();
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+    let mut delay = Delay::new(core_peripherals.SYST, clocks);
+    let mut gpiob = peripherals.GPIOB.split(&mut rcc.ahb);
+
+    // Configure the I2C pins and bus
+    //    "moder": ??
+    //    "afrl": alternate function low...?
+    let scl = gpiob.pb6.into_af4(&mut gpiob.moder, &mut gpiob.afrl);
+    let sda = gpiob.pb7.into_af4(&mut gpiob.moder, &mut gpiob.afrl);
+    let i2c = i2c::I2c::new(peripherals.I2C1, (scl, sda), 400.khz(), clocks, &mut rcc.apb1);
+
+
     loop {
         // your code goes here
+        hprintln!(".").unwrap();
+        delay.delay_ms(1000_u16);
     }
 }
